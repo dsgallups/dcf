953bf48b4ec98547cef351139fadef6f762a6247
dsgallups <dsgallups@protonmail.com>
2025-11-25 15:56:05 -0500

    add naive trait

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,3 +1,19 @@
 use dcf::*;
 
+pub struct SimpleStruct {
+    value: bool,
+}
+
+impl<'a> Dcf<'a> for SimpleStruct {
+    fn serialize(&self) -> Vec<u8> {
+        todo!()
+    }
+    fn deserialize(bytes: &'a [u8]) -> Self
+    where
+        Self: 'a,
+    {
+        todo!()
+    }
+}
+
 fn main() {}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,6 @@
-
+pub trait Dcf<'a> {
+    fn serialize(&self) -> Vec<u8>;
+    fn deserialize(bytes: &'a [u8]) -> Self
+    where
+        Self: 'a;
+}
