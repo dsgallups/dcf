39281df9c989b1556f7fff6180c616bf6d35e5a7
dsgallups <dsgallups@protonmail.com>
2025-11-29 12:46:18 -0500

    feat: good bit packing. nice.

--- examples/tiny.rs
+++ examples/tiny.rs
@@ -5,61 +5,63 @@ pub struct TinyWriter {
     current_bit: u8,
 }
 
+/// Returns a bit with information shifted to the left
+///
+/// (byte, num_valuable_bits)
+fn shift(bit: u8) -> (u8, u8) {
+    if bit == 0 {
+        (0, 1)
+    } else {
+        let leading_zeroes = bit.leading_zeros() as u8;
+        (bit << bit.leading_zeros(), 8 - leading_zeroes)
+    }
+}
+
 impl TinyWriter {
+    pub fn insert_shifted_bit(&mut self, shifted: u8, bits_occupied: u8) {
+        self.current_bit |= shifted >> self.current_bit_cursor;
+
+        self.current_bit_cursor += bits_occupied;
+
+        if self.current_bit_cursor < 8 {
+            return;
+        }
+
+        let remainder_bits_occupied = self.current_bit_cursor - 8;
+
+        self.stack.push(self.current_bit);
+        self.current_bit_cursor = 0;
+        self.current_bit = 0;
+        if remainder_bits_occupied == 0 {
+            return;
+        }
+
+        let remainder_bits = shifted << remainder_bits_occupied;
+        self.insert_shifted_bit(remainder_bits, 8 - remainder_bits_occupied);
+    }
     pub fn insert_bitlen(&mut self, bit: u8) {
-        let mut leading_zeros = bit.leading_zeros() as u8;
-        if bit == 0 {
-            leading_zeros = 7;
-        }
-        /*
-         000000
-         push 10
-         100000
-         push 0
-         100000
-         push 1
-         100100
-         push 1
-         100110
-        */
-
-        println!(
-            "leading_zeros: {}, cursor: {}",
-            leading_zeros, self.current_bit_cursor
-        );
-        let mut shift = leading_zeros as i8 - self.current_bit_cursor as i8;
-
-        let right_cursor = self.current_bit_cursor + leading_zeros;
-        let remainder = right_cursor.saturating_sub(8);
-        if remainder > 0 {
-            //todo
-        }
-
-        println!(
-            "left_cursor: {}, right_cursor: {}, remainder: {},  shift: {}",
-            8 - right_cursor as i8,
-            right_cursor,
-            remainder,
-            shift
-        );
-
-        self.current_bit |= bit << shift;
-
-        self.current_bit_cursor += 8 - leading_zeros;
-        //let cursor = self.current_bit_cursor;
-
-        //self.current_bit_cursor = cursor & (bit << 7);
-        //self.current_bit = ();
+        let (shifted, bits_occupied) = shift(bit);
+        self.insert_shifted_bit(shifted, bits_occupied);
+        println!("leading zeros: {}", bit.leading_zeros());
 
         println!(
             "Bit: {bit:08b}\nRes: {:08b}\nCur: {}\n==========",
             self.current_bit, self.current_bit_cursor
         );
-        //panic!("leading zeros: {leading_zeros}");
+    }
+
+    pub fn finish(mut self) -> Vec<u8> {
+        self.stack.push(self.current_bit);
+        self.stack
     }
 }
 
 fn main() {
+    let bit: u8 = 0b00101;
+    let bit_after = bit << bit.leading_zeros() as u8;
+
+    println!("bit: {:08b}, aft: {:08b}", bit, bit_after);
+
     let mut w = TinyWriter::default();
     w.insert_bitlen(1);
     w.insert_bitlen(0);
@@ -71,5 +73,9 @@ fn main() {
     w.insert_bitlen(0b1011);
 
     assert_eq!(w.stack[0], 0b1011_0110);
-    assert_eq!(w.current_bit, 0b1100_0000)
+    assert_eq!(w.current_bit, 0b1100_0000);
+
+    let bytes = w.finish();
+
+    println!("Success encoding! {bytes:?}");
 }
